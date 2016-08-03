26d05b93634a18ec9d3078d3731889d8efba371c
NadegdaMu <bykovanadegda@gmail.com>
2016-08-03 21:33:17 +0600

    верстка

---
+++ js/script.js
@@ -0,0 +1,60 @@
+ //Модальное окно формы.
+
+  "use strict";
+
+  var link = document.querySelector(".contacts__btn");
+  var popup = document.querySelector(".modal-content");
+  var close = popup.querySelector(".modal-content__close");
+  var login = popup.querySelector("[name=login-name]");
+  var form = popup.querySelector("form");
+  var email = popup.querySelector("[name=email]");
+  var storage = window.localStorage;
+ 
+  
+  link.addEventListener("click", function(event) {
+       event.preventDefault();
+       popup.classList.add("modal-content--show");
+
+       if (storage) {
+          login.value = storage.getItem("login");
+          email.focus();
+        } else {
+          login.value="";
+          login.focus();
+        }
+
+       login.focus();
+      });
+
+  close.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("modal-content--show");
+        popup.classList.remove("modal-error");
+      });
+
+  form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        if (!login.value || !email.value) {
+          // event.preventDefault();
+          popup.classList.remove("modal-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("modal-error");
+           
+        } else {
+          
+          storage.setItem("login",login.value);
+          popup.classList.remove("modal-content--show");
+      }});
+
+  window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (popup.classList.contains("modal-content--show")) {
+            popup.classList.remove("modal-content--show");
+            popup.classList.remove("modal-error");
+          }
+        }
+      });
+
+ 
+
